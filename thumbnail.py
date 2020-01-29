03de34ca66d94d3d69fdee4c6e1ef9db3d44830d
Alex Meade <alex@developintelligence.com>
2020-01-28 20:19:51 -0500

    Cleanup script

--- thumbnail.py
+++ thumbnail.py
@@ -1,16 +1,10 @@
 from PIL import Image
 from PIL import _imaging
-import boto3  
 import os  
-import re  
-import subprocess
-
-s3 = boto3.client('s3')  
 
 max_width = 256
 max_height = 256
 image_format = "jpg"
-acl = "public-read"
 output_dir = "/tmp/thumbnails"
 output_bucket = "ameade-thumbnail-images"
 
@@ -19,34 +13,25 @@ def handler(event, context):
   key = event['object']
 
   try:
-    meta = metadata(bucket, key)
-    # skip if image is already processed
-    if 'image-processed' in meta and meta['image-processed'] == 'true':
-      raise Exception('Already processed image')
-
-    tmp = tmp_path(key)
-    s3.download_file(Bucket=bucket, Key=key, Filename=tmp)
+    #tmp = "/tmp/" + key
+    tmp = key
+    download(bucket, key, tmp)
 
     # resize
     resized = resized_path(key)
     resize(tmp, resized)
 
     # upload resized image
-    upload(resized, bucket, key)
+    upload(resized, output_bucket, key)
 
     return  
 
   except Exception as e:  
       print(e)  
       
-
-def tmp_path(key):
-  filename = os.path.basename(key)  
-  return u'/tmp/' + filename 
-
 def resized_path(key):
-  filename = os.path.basename(key)  
-  return '/tmp/{}.{}'.format(filename, image_format)
+  name = get_filename(os.path.basename(key))
+  return '/tmp/{}.{}'.format(name, image_format)
 
 def resize(src, dest):
   # pil image processing
@@ -57,31 +42,18 @@ def resize(src, dest):
     img = img.convert('RGB')
   print('image resized')
   return img.save(dest)
-  
-def metadata(bucket, key):
-  return s3.head_object(Bucket=bucket, Key=key)['Metadata']
 
-def download(bucket, key, dest):
-  return s3.download_file(Bucket=bucket, Key=key, Filename=dest)  
+def download(source_bucket, source_object_key, tmp):
+  """ Download the specified object to the tmp location """
+  # TODO
+  pass
 
-def upload(src, bucket, dest):
-  return s3.upload_file(Filename=src, Bucket=dest_bucket(bucket), Key=dest, ExtraArgs=extra_args())
+def upload(src, dest_bucket, dest_object):
+  """ Upload the specified src file to the specific object"""
+  # TODO
+  pass
 
-def dest_bucket(bucket):
-  return bucket if not output_bucket else output_bucket
-
-def extra_args():
-  return {'ContentType': content_type(), 'Metadata': {'image-processed': 'true'}, 'ACL': acl}
-
-def content_type():
-  if image_format in ['jpg', 'jpeg']:
-    return 'image/jpeg'
-  elif image_format == 'png':
-    return 'image/png'
-  else:
-    return 'application/octet-stream'
-
-def filename(key):
+def get_filename(key):
   filename = os.path.basename(key)  
   name, ext = os.path.splitext(filename)
   return name
